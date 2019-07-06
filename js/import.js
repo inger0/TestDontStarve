f780ed3f9e07831d78edc82b00d1c16399d415e2
zhaoshuai <zhaohongshuai@ke.com>
2019-07-06 18:52:38 +0800

    footer module

--- js/import.js
+++ js/import.js
@@ -1,6 +1,11 @@
 $(document).ready(function ($) {
     $(document).ready(function () {
+        // 菜单引用
         $('#menu').empty();
         $('#menu').load('module-menu.html');
-    });
+
+        // Footer引用
+        $('#footer').empty();
+        $('#footer').load('module-footer.html');
+    });it
 });
