edeecc6586d4e3fcc3e75210c63e00bc9dafd65d
zhaoshuai <zhaohongshuai@ke.com>
2019-07-10 15:13:06 +0800

    视频 本地缓存

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
-
 getTitle();
+
 function getTitle() {
     var storage = window.sessionStorage;
     var dataInCache = storage.getItem("indexTitle");//localStorage取数据
@@ -7,14 +7,17 @@ function getTitle() {
     if (dataInCache !== null) {	//localStorage有数据则直接渲染
         $("#index-title").html(dataInCache);
     } else {
-        $.getJSON("storage/index.json", function (data) {
-            data.sort(function () {
-                return (0.5 - Math.random());
-            });
-            storage.setItem("indexTitle", data[0]);//放入localStorage，名字为renderRoute。
+        data = [
+            "About Don't Starve",
+            "About LOL",
+            "About Test"
+        ];
+        data.sort(function () {
+            return (0.5 - Math.random());
+        });
+        storage.setItem("indexTitle", data[0]);//放入localStorage，名字为renderRoute。
 
-            $("#index-title").html(data[0]);
-        })
+        $("#index-title").html(data[0]);
     }
 }
 
